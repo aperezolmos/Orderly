17dcde39272a587ead5bb4902fab944a409d98da
aperezolmos <amandaprzolmos@gmail.com>
2025-10-02 13:20:16 +0200

    feat(user): Crear servicio para usuarios y UserMapper
    - Se ha creado la clase UserMapper para no delegar la función de convertir las entidades a DTO en el UserService (cumpliendo así el SRP).
    - Queda pendiente el método 'update' para los usuarios.
    
    Refs #18

--- spring-boot/src/main/java/es/ubu/inf/tfg/user/UserRepository.java
+++ spring-boot/src/main/java/es/ubu/inf/tfg/user/UserRepository.java
@@ -1,10 +1,12 @@
 package es.ubu.inf.tfg.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {   
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
 }
---
+++ spring-boot/src/main/java/es/ubu/inf/tfg/user/mapper/UserMapper.java
@@ -0,0 +1,25 @@
+package es.ubu.inf.tfg.user.mapper;
+
+import org.springframework.stereotype.Component;
+
+import es.ubu.inf.tfg.user.User;
+import es.ubu.inf.tfg.user.dto.UserResponseDTO;
+
+@Component
+public class UserMapper {
+    
+    public UserResponseDTO toResponseDTO(User user) {
+        if (user == null) return null;
+        
+        return UserResponseDTO.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .roleName(user.getRole().getName())
+                .createdAt(user.getCreatedAt())
+                .build();
+    }
+    
+    //TODO: toentity¿?
+}
