f0a69be492bdaebc04b32b334513b82bb52c9d98
aperezolmos <amandaprzolmos@gmail.com>
2025-11-11 10:42:00 +0100

    feat: Crear entidad DiningTable
    Esta entidad representa las mesas del comedor, que podrán ser reservadas.
    - La relación con Reservation será unidireccional, con Reservation como propietaria (no añadimos campo 'reservations' en DiningTable).
    - El 'tableNumber' se ha puesto como String por si se quisiera utilizar un identificador personalizado (ej: M01, BAR03...).
    
    Refs #29

---
+++ app/spring-boot/src/main/java/es/ubu/inf/tfg/reservation/diningTable/DiningTable.java
@@ -0,0 +1,47 @@
+package es.ubu.inf.tfg.reservation.diningTable;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+
+import lombok.*;
+
+@Entity
+@Table(name = "dining_tables")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
+@Builder
+public class DiningTable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @ToString.Include
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 10)
+    @ToString.Include
+    private String tableNumber;
+
+    @Column(nullable = false)
+    private Integer capacity;
+
+    @Column(length = 100)
+    private String locationDescription;
+    
+    @Builder.Default
+    private Boolean isActive = true;
+    
+    private LocalDateTime createdAt;
+
+    // --------------------------------------------------------
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now().plusHours(2); 
+    }
+}
