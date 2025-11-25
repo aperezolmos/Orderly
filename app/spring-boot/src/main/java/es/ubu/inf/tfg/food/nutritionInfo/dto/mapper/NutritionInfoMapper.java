5a1edab62cdec25a2b50f48a5588cfb33815e9c8
aperezolmos <amandaprzolmos@gmail.com>
2025-11-25 20:24:20 +0100

    feat(test): Crear tests para mapper y servicio de Food
    Refs #98

--- app/spring-boot/src/main/java/es/ubu/inf/tfg/food/nutritionInfo/dto/mapper/NutritionInfoMapper.java
+++ app/spring-boot/src/main/java/es/ubu/inf/tfg/food/nutritionInfo/dto/mapper/NutritionInfoMapper.java
@@ -4,19 +4,11 @@ import es.ubu.inf.tfg.food.nutritionInfo.NutritionInfo;
 import es.ubu.inf.tfg.food.nutritionInfo.dto.NutritionInfoDTO;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public abstract class NutritionInfoMapper {
 
-    @Mapping(target = "add", ignore = true)
-    @Mapping(target = "multiplyByFactor", ignore = true)
-    @Mapping(target = "minerals.add", ignore = true)
-    @Mapping(target = "minerals.multiplyByFactor", ignore = true)
-    @Mapping(target = "vitamins.add", ignore = true)
-    @Mapping(target = "vitamins.multiplyByFactor", ignore = true)
     public abstract NutritionInfo toEntity(NutritionInfoDTO dto);
-
 
     public abstract NutritionInfoDTO toDTO(NutritionInfo entity);
 }
